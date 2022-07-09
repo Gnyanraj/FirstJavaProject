f3e33064656fff1b233cb0519105fb45670c9a34
Gnyanraj <33523727+Gnyanraj@users.noreply.github.com>
2022-07-09 20:45:03 +0530

    Updated

--- HelloGnyan.java
+++ HelloGnyan.java
@@ -1,5 +1,5 @@
-public class Hello {
-  public static void main(String args[]){
+public class HelloGnyan {
+  public static void main(String[] args){
     System.out.println("Hello from Gnyan First Java Project...");
   }
 }
