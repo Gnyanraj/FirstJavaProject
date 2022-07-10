70befbd3dfb5b1024ee2f849c6aee219c80fe827
Gnyanraj <33523727+Gnyanraj@users.noreply.github.com>
2022-07-10 16:26:28 +0530

    Poll SCM

--- HelloGnyan.java
+++ HelloGnyan.java
@@ -1,6 +1,7 @@
 public class HelloGnyan {
   public static void main(String[] args){
     System.out.println("Hello from Gnyan First Java Project...");
+    System.out.println("Cheking from Jenkins with Poll SCM option");
   }
 }
     
